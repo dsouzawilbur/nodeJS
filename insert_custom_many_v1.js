1cecb8367be7c8c6f833df3093e3457e78fa080d
dsouzawilbur <dsouza.wilbur@gmail.com>
2021-09-18 22:43:41 +0530

    Create insert_custom_many_v1.js

---
+++ insert_custom_many_v1.js
@@ -0,0 +1,18 @@
+var MongoClient = require('mongodb').MongoClient;
+var url = "URI";
+
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("sample_airbnb");
+  var myobj = [
+    { name: "Example 1", address: "Highway 1", documentType: null , personalDetails: { gender: "male", age: 23, position: null }},
+    { name: "Example 2", address: "Highway 2", documentType: "details" , personalDetails: { gender: "male", age: 23, position: null }},
+    { name: "Example 2", documentType: "details" , personalDetails: { gender: "male", age: 23, position: null,  address: { street: "NYJ", pincode: "5555", GPS: { LAT: "111.11", LON: "222.22"}}}}
+  ];
+  dbo.collection("customers").insertMany(myobj, function(err, res) {
+    if (err) throw err;
+    console.log("1 document inserted");
+    db.close();
+  });
+});
+
