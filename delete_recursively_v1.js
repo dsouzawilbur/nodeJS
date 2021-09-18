8c2778ff3938cae91eb513826cdc99ffac6fc0ca
dsouzawilbur <dsouza.wilbur@gmail.com>
2021-09-18 22:41:48 +0530

    Update delete_recursively_v1.js

--- delete_recursively_v1.js
+++ delete_recursively_v1.js
@@ -86,104 +86,58 @@ docKeysArray = db.customers.aggregate([
         }
       }
   }},
-
   {$addFields: {
-
       data: {$reduce: {
-
           input: "$data",
-
           initialValue: [],
-
           in: {$concatArrays: ["$$value", "$$this.v"]}
-
       }}
-
   }},
-
   {$project: {
-
      fields: {$concatArrays: ["$fields","$data.k"]}
-
   }}
-
 ]).toArray();
 
-
 async function asyncForEach(array, callback) {
-
   for (let index = 0; index < array.length; index++) {
-
     await callback(array[index], index, array);
-
   }
-
 }
 
 
 asyncForEach(docKeysArray, async (docKeys) => {
-
     //console.log(JSON.stringify(docKeys));
-
     doc = db.customers.findOne({_id : ObjectId(docKeys._id)});
-
     //console.log(JSON.stringify(doc));
-
     if (doc.name == 'Example 1') {
-
         fieldsArray = ["address", "gender"];
-
         //console.log("Inside 1");
-
     }
-
     else if (doc.name == 'Example 2') {
-
         fieldsArray = ["address", "age"];
-
         //console.log("Inside 2");
-
-
     }
 
     else if (doc.name == 'Example 3') {
-
         fieldsArray = ["LAT", "LON"];
-
         //console.log("Inside 3");
-
     }
 
     fieldsArray.forEach(function (field) {
-
         //console.log('.*' + field + '$');
-
         fieldRegex = new RegExp('.*' + field + '$');
-
         docKeys.fields.forEach(function (docKey) {
-
             if ((fieldRegex.test(docKey))) {
-
                 db.customers.updateOne(
-
                     { 
-
                         _id : ObjectId(doc._id) 
-
                     }, 
-
                     { 
-
                         $unset: 
-
                         { 
-
                             [docKey]: "" 
-
                         } 
-
                     }
-
                 );
             }
         });
